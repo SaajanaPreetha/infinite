6ccea6bdf22b9249c06a09a73742691996063c01
Saajana Preetha <saajanap@infinite.com>
2024-06-20 09:13:08 +0530

    assignment2

---
+++ C#/Assignments/Assignment2/assign2ques3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign2ques3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the number of elements you want in the array:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Enter element {i + 1}:");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            double average = array.Average();
+            int min = array.Min();
+            int max = array.Max();
+
+            Console.WriteLine("Array elements:");
+            Console.WriteLine($"Average value of array elements: {average}");
+            Console.WriteLine($"Minimum value in array: {min}");
+            Console.WriteLine($"Maximum value in array: {max}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assign2ques6/assign2ques6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign2ques6
+{
+    class Program
+    {
+        static void Main()
+        {
+            {
+                Console.WriteLine("Enter a word:");
+                string word = Console.ReadLine();
+                int length = word.Length;
+                Console.WriteLine($"The length of '{word}' is: {length}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
