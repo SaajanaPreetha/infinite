85222edd3d3735b72089a407e0a352b2f6c71918
Saajana Preetha <saajanap@infinite.com>
2024-08-07 11:25:48 +0530

    webtech

---
+++ web technologies/web assessments/web assessment1/web assessment1ques1/web assessment1ques1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web_assessment1ques1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
